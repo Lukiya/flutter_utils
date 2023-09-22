4c15983db9d1c107667e3342bc092fc099ee9d81
Lukiya <lukiya@outlook.com>
2023-09-22 08:08:26 -0700

    u

--- lib/dreamvat.io.dart
+++ lib/dreamvat.io.dart
@@ -1,44 +1,44 @@
-import 'dart:io';
-// import 'dart:typed_data';
+// import 'dart:io';
+// // import 'dart:typed_data';
 
-import 'package:archive/archive_io.dart';
-import 'package:flutter/foundation.dart';
-// import 'package:dreamvat/dreamvat.dart';
+// import 'package:archive/archive_io.dart';
+// import 'package:flutter/foundation.dart';
+// // import 'package:dreamvat/dreamvat.dart';
 
-Future<Uint8List> compress(Map<String, dynamic> args) async {
-  final srcPath = args["srcPath"];
-  final tmpPath = args["tmpPath"];
+// Future<Uint8List> compress(Map<String, dynamic> args) async {
+//   final srcPath = args["srcPath"];
+//   final tmpPath = args["tmpPath"];
 
-  try {
-    var encoder = ZipFileEncoder(); // 压缩器
-    encoder.create(tmpPath); // 创建压缩文件
-    encoder.addFile(File(srcPath)); // 添加数据库文件进zip文件
-    encoder.close(); // 关闭压缩器
+//   try {
+//     var encoder = ZipFileEncoder(); // 压缩器
+//     encoder.create(tmpPath); // 创建压缩文件
+//     encoder.addFile(File(srcPath)); // 添加数据库文件进zip文件
+//     encoder.close(); // 关闭压缩器
 
-    final zipFile = File(tmpPath); // zip文件
-    final r = await zipFile.readAsBytes(); // 读取压缩文件数据
-    await zipFile.delete(); // 删除zip文件
-    return r;
-  } catch (err) {
-    // Logger.error("Compress db failed", error: err, stackTrace: StackTrace.current);
-    debugPrint("Compress db failed, $err, ${StackTrace.current}");
-  }
+//     final zipFile = File(tmpPath); // zip文件
+//     final r = await zipFile.readAsBytes(); // 读取压缩文件数据
+//     await zipFile.delete(); // 删除zip文件
+//     return r;
+//   } catch (err) {
+//     // Logger.error("Compress db failed", error: err, stackTrace: StackTrace.current);
+//     debugPrint("Compress db failed, $err, ${StackTrace.current}");
+//   }
 
-  return Uint8List(0);
-}
+//   return Uint8List(0);
+// }
 
-Future<void> decompress(Map<String, dynamic> args) async {
-  final data = args["data"];
-  final tarPath = args["tarPath"];
+// Future<void> decompress(Map<String, dynamic> args) async {
+//   final data = args["data"];
+//   final tarPath = args["tarPath"];
 
-  try {
-    // 是压缩文件
-    final archive = ZipDecoder().decodeBytes(data); // 解压缩
-    final zipFile = archive.first; // 获取第一个文件
-    final targetFile = File(tarPath);
-    await targetFile.writeAsBytes(zipFile.content); // 写入数据库文件
-  } catch (err) {
-    debugPrint("Compress db failed, $err, ${StackTrace.current}");
-    // Logger.error("Decompress db failed", error: err, stackTrace: StackTrace.current);
-  }
-}
+//   try {
+//     // 是压缩文件
+//     final archive = ZipDecoder().decodeBytes(data); // 解压缩
+//     final zipFile = archive.first; // 获取第一个文件
+//     final targetFile = File(tarPath);
+//     await targetFile.writeAsBytes(zipFile.content); // 写入数据库文件
+//   } catch (err) {
+//     debugPrint("Compress db failed, $err, ${StackTrace.current}");
+//     // Logger.error("Decompress db failed", error: err, stackTrace: StackTrace.current);
+//   }
+// }
--- lib/widgets/sync/onedrive.dart
+++ lib/widgets/sync/onedrive.dart
@@ -1,84 +1,84 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_onedrive/flutter_onedrive.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_onedrive/flutter_onedrive.dart';
+// import 'package:flutter_slidable/flutter_slidable.dart';
 
-class OneDriveListTile extends StatefulWidget {
-  final OneDrive onedrive;
-  final String? disconnectText;
-  final String? connectedText;
-  const OneDriveListTile(
-    this.onedrive, {
-    this.disconnectText = "Disconnect",
-    this.connectedText = "Connected",
-    Key? key,
-  }) : super(key: key);
+// class OneDriveListTile extends StatefulWidget {
+//   final OneDrive onedrive;
+//   final String? disconnectText;
+//   final String? connectedText;
+//   const OneDriveListTile(
+//     this.onedrive, {
+//     this.disconnectText = "Disconnect",
+//     this.connectedText = "Connected",
+//     Key? key,
+//   }) : super(key: key);
 
-  @override
-  State<StatefulWidget> createState() => OneDriveListTileStatue();
-}
+//   @override
+//   State<StatefulWidget> createState() => OneDriveListTileStatue();
+// }
 
-class OneDriveListTileStatue extends State<OneDriveListTile> {
-  static const name = "OneDrive";
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: widget.onedrive.isConnected(),
-      builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
-        if (snapshot.connectionState != ConnectionState.done || !snapshot.hasData) {
-          return const ListTile(
-            title: Text(name),
-            trailing: SizedBox(
-              width: 15,
-              height: 15,
-              child: CircularProgressIndicator(),
-            ),
-          );
-        }
+// class OneDriveListTileStatue extends State<OneDriveListTile> {
+//   static const name = "OneDrive";
+//   @override
+//   Widget build(BuildContext context) {
+//     return FutureBuilder(
+//       future: widget.onedrive.isConnected(),
+//       builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+//         if (snapshot.connectionState != ConnectionState.done || !snapshot.hasData) {
+//           return const ListTile(
+//             title: Text(name),
+//             trailing: SizedBox(
+//               width: 15,
+//               height: 15,
+//               child: CircularProgressIndicator(),
+//             ),
+//           );
+//         }
 
-        if (snapshot.data ?? false) {
-          return Slidable(
-// 左滑显示断开按钮
-            endActionPane: ActionPane(
-              motion: const DrawerMotion(),
-              extentRatio: .30,
-              children: <Widget>[
-                SlidableAction(
-                  label: widget.disconnectText,
-                  backgroundColor: Colors.red,
-                  icon: Icons.delete,
-                  onPressed: (_) async {
-                    // 点击断开连接
-                    await widget.onedrive.disconnect();
-                    setState(() {});
-                  },
-                ),
-              ],
-            ),
-            child: Container(
-              decoration: const BoxDecoration(
-                border: Border(bottom: BorderSide(width: 1, color: Color(0xFFCCCCCC))),
-              ),
-              child: ListTile(
-                title: const Text(name),
-                subtitle: Text(widget.connectedText ?? ""),
-                trailing: const Icon(Icons.check_circle, color: Colors.lightGreen),
-              ),
-            ),
-          );
-        } else {
-          return ListTile(
-            title: const Text(name),
-            trailing: const Icon(Icons.chevron_right),
-            onTap: () async {
-// 点击连接
-              final success = await widget.onedrive.connect(context);
-              if (success) {
-                setState(() {});
-              }
-            },
-          );
-        }
-      },
-    );
-  }
-}
+//         if (snapshot.data ?? false) {
+//           return Slidable(
+// // 左滑显示断开按钮
+//             endActionPane: ActionPane(
+//               motion: const DrawerMotion(),
+//               extentRatio: .30,
+//               children: <Widget>[
+//                 SlidableAction(
+//                   label: widget.disconnectText,
+//                   backgroundColor: Colors.red,
+//                   icon: Icons.delete,
+//                   onPressed: (_) async {
+//                     // 点击断开连接
+//                     await widget.onedrive.disconnect();
+//                     setState(() {});
+//                   },
+//                 ),
+//               ],
+//             ),
+//             child: Container(
+//               decoration: const BoxDecoration(
+//                 border: Border(bottom: BorderSide(width: 1, color: Color(0xFFCCCCCC))),
+//               ),
+//               child: ListTile(
+//                 title: const Text(name),
+//                 subtitle: Text(widget.connectedText ?? ""),
+//                 trailing: const Icon(Icons.check_circle, color: Colors.lightGreen),
+//               ),
+//             ),
+//           );
+//         } else {
+//           return ListTile(
+//             title: const Text(name),
+//             trailing: const Icon(Icons.chevron_right),
+//             onTap: () async {
+// // 点击连接
+//               final success = await widget.onedrive.connect(context);
+//               if (success) {
+//                 setState(() {});
+//               }
+//             },
+//           );
+//         }
+//       },
+//     );
+//   }
+// }
